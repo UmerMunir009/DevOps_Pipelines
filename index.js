090b03f5b8168beb6f0f180d611686033778a7e4
Umer Munir <umermunir414@gmail.com>
2025-09-30 12:36:30 +0500

    First pipeline created

---
+++ index.js
@@ -0,0 +1,1 @@
+console.log('CI/CD pipelines using Github Actions')
---
+++ test.js
@@ -0,0 +1,3 @@
+console.log('Testing.....')
+setTimeout(()=>console.log("Waiting 5 seconds..."),5000)
+console.log('Test Completed')
